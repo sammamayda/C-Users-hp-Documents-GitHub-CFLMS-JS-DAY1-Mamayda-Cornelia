76e49e463328008ca7273cda3f5fc72914d36f4e
Cornelia Franta <franta-cornelia@gmx.at>
2020-05-25 13:20:55 +0200

    editing index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Hello my name is Cornelia');
+
+var fruits = ['apple', 'banana', 'kiwi'];
+
+fruits.push('orange');
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+var animals = ['monkey', 'horse', 'dog'];
+
+console.log(animals.sort());
+animals.unshift('cat');
+console.log(animals);
+
+
+var myString = 'mango/cherries/kiwi/grapes/pear/peach/orange/lemon';
+console.log(myString.split('/').join('\n'));
