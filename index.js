b88a20e7eeadbcb738861baf23affadf94ce8f9e
sammamayda <samsamwel29.gmail.com>
2020-05-25 14:22:03 +0200

    initila it

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ var c = "21";
 var d = '36';
 var e = 42;
 
-var result1 = a + b+ Number(c) + Number(d) + e;
+var result1 = a + b + Number(c) + Number(d) + e;
 console.log(result1);
 
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+var result2 = Number(f) * g * h * Number(i);
+console.log(result2)
+
+console.log(result1 / result2);
